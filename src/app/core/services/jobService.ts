7762e1c4f80b6974aed82d0e5d5191f08c177074
mukhtar <mukhtar.ali17@gmail.com>
2026-04-19 03:58:00 +0500

    Job service

---
+++ src/app/core/services/jobService.ts
@@ -0,0 +1,54 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Job, JobResponse } from '../models/job.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class JobService {
+  
+  private apiUrl = 'https://remotive.com/api/remote-jobs';
+
+  constructor(private http: HttpClient) {}
+
+  getJobs():Observable<Job[]> {
+    return this.http.get<JobResponse>(this.apiUrl).pipe(
+      map(response => response.jobs)
+    );
+  }
+
+  getFeaturedJobs(limit: number = 6):Observable<Job[]> {
+    return this.http.get<JobResponse>(this.apiUrl).pipe(
+      map(response => response.jobs.slice(0,limit))
+    );
+  }
+
+  getJobsByCategory(category: string):Observable<Job[]> {
+    const params = new HttpParams().set('category', category);
+
+    return this.http.get<JobResponse>(this.apiUrl, {params}).pipe(
+      map(response => response.jobs)
+    );
+  }
+
+  searchJobs(searchTerm: string):Observable<Job[]> {
+    const params = new HttpParams().set('search', searchTerm);
+    return this.http.get<JobResponse>(this.apiUrl, {params}).pipe(
+      map(response => response.jobs)
+    );
+  }
+
+  getCategories(): {name: string; icon: string; slug: string}[] {
+    return [
+      { name: 'Software Development', icon: '💻', slug: 'software-dev' },
+      { name: 'Design', icon: '🎨', slug: 'design' },
+      { name: 'Marketing', icon: '📢', slug: 'marketing' },
+      { name: 'Customer Service', icon: '🎧', slug: 'customer-support' },
+      { name: 'Sales', icon: '💼', slug: 'sales' },
+      { name: 'Data', icon: '📊', slug: 'data' },
+      { name: 'DevOps', icon: '⚙️', slug: 'devops' },
+      { name: 'Writing', icon: '✍️', slug: 'writing' }
+    ]
+  }
+}
