5deefdb57dc2a2bb82587115189f5f145d7667d8
mukhtar <mukhtar.ali17@gmail.com>
2026-04-19 23:32:10 +0500

    add layout with navbar main and footer

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../../shared/components/navbar/navbar";
+import { Footer } from "../../shared/components/footer/footer";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-main-layout',
-  imports: [],
+  imports: [Navbar, Footer, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
